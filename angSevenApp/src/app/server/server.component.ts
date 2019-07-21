316bd87a074a1ef7e75b661e5033befa48de7a7a
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2019-07-21 17:59:51 +0530

    moved the server component in a separate folder inside the app

--- angSevenApp/src/app/server/server.component.ts
+++ angSevenApp/src/app/server/server.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.componrnt.html'
+    templateUrl: './server.component.html'
 })
 export class ServerComponent { // exporting the class to be able to use it outside of this file
 
