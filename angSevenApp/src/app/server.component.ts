78eef9a01ba41612d0d466bcf6e5d88f75fc9be0
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2019-07-21 16:15:04 +0530

    created a server component and the template html file

---
+++ angSevenApp/src/app/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.componrnt.html'
+})
+export class ServerComponent { // exporting the class to be able to use it outside of this file
+
+}
