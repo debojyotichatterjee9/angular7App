1d5e2bb8b7bed7ccda763466b92b1225287a2751
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2019-07-21 23:55:54 +0530

    added the server component via cli command

---
+++ angSevenApp/src/app/servers/servers.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line
+  // selector: '[app-servers]', // checking component selector with attribute selector
+  // tslint:disable-next-line
+  // selector: '.app-servers', // checking component selector with css selector
+  selector: 'app-servers', // checking component selector with element selector
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
