748458916fa1211b154fddc6df79f58b8de95efc
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2019-07-21 21:29:45 +0530

    working of string interpolation and data binding

--- angSevenApp/src/app/server/server.component.ts
+++ angSevenApp/src/app/server/server.component.ts
@@ -5,5 +5,10 @@ import { Component } from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent { // exporting the class to be able to use it outside of this file
+serverId: number = 10;
+serverStatus: string = 'Offline';
 
+getServerStatus() {
+    return this.serverStatus;
+}
 }
