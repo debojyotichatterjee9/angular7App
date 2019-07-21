3dd18ebd463403a059fcf36ca35f223e54d12ae4
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2019-07-21 21:12:27 +0530

    checking the different selectors by attribute, class and element

--- angSevenApp/src/app/app.component.ts
+++ angSevenApp/src/app/app.component.ts
@@ -8,3 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'changedAppTitle';
 }
+/* in the decorator section we can use template instead to templateUrl to write the html here itself,
+if we need to write multiline HTML the we can use the tilt symbol and wrap the whole thing in it
+the styleUrls can also be similarly replaced with styles and not the this uses an array so that means we can
+use multiple sile pointer references to the styleUrls and or styles*/
