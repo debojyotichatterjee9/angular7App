28b646ccab63126779e5a9c5da0d6cee7ea6bcb7
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2019-08-11 23:59:01 +0530

    recipe list component modified and added dummy recipe for checking

--- angSevenApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angSevenApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes = [];
+  recipes: Recipe[] = [ // the only thing that will grt stored in this recipes propety is an array which holds Recipe object
+    new Recipe('Test Recipe', 'Test Decription', 'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1553&q=80')
+  ]; 
   constructor() { }
 
   ngOnInit() {
