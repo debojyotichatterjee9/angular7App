643744343fe66a362f885af9ee5d0d679f4f1a3f
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-05-05 21:34:40 +0530

    checking data bindings

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,13 +4,14 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
     serverId: number = 300;
     serverStatus: string = 'Offline';
 
-getServerStatus() {
+getServerStatus(): string {
     return this.serverStatus
 }
 }
