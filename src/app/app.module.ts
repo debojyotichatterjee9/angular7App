d0b8b58b0581f01cac3a9ed26f14f69e79d27c43
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-05-06 18:11:54 +0530

    cleaning up test files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessComponent } from './success/success.component'
-import { WarningComponent } from './warning/warning.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    SuccessComponent,
-    WarningComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
