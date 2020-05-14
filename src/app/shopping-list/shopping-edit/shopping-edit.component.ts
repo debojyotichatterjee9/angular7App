97ef4430b4003508b25d5f1a9b9ec88857d6599c
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-05-14 22:09:36 +0530

    Allowing the User to Add Ingredients to the Shopping List :building_construction:
    delete and cleat options are not implemented atm (need work)

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from "../../shared/ingredient.model"
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+    
+    @ViewChild('nameInput', {static: true}) nameInputRef: ElementRef;
+    @ViewChild('quantityInput', {static: true}) quantityInputRef: ElementRef;
+    @Output('addIngredinetEmit') ingredientAdded = new EventEmitter<Ingredient>();
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+    
+    onAddItem() {
+        const ingredientName = this.nameInputRef.nativeElement.value;
+        const ingredientQuantity = this.quantityInputRef.nativeElement.value;
+        const ingredient = new Ingredient(ingredientName, ingredientQuantity)
+        this.ingredientAdded.emit(ingredient);
+    }
 
+    onDeleteItem() {
+        console.warn("Not Implemented...")
+    }
+    
+    onClearItem() {
+        console.warn("Not Implemented...")
+    }
 }
